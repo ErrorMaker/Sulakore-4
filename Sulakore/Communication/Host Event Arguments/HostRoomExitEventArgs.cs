fac36168edfb2bfb7f7cec862cc9eeca04f4b55a
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:15:53 -0800

    Renamed HostExitEventArgs to HostRoomExitEventArgs.

---
+++ Sulakore/Communication/Host Event Arguments/HostRoomExitEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sulakore.Protocol;
+
+namespace Sulakore.Communication
+{
+    public class HostRoomExitEventArgs : EventArgs, IHabboEvent
+    {
+        private readonly HMessage _packet;
+
+        public ushort Header { get; private set; }
+
+        public HostRoomExitEventArgs(HMessage packet)
+        {
+            _packet = packet;
+            Header = _packet.Header;
+        }
+    }
+}
