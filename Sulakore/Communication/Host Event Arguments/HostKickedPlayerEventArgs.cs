a1eff4bc2819e2cdc3eefe1205a8b1b2081cdae5
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:15:09 -0800

    Renamed HostKickPlayerEventArgs to HostKickedPlayerEventArgs.

---
+++ Sulakore/Communication/Host Event Arguments/HostKickedPlayerEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sulakore.Protocol;
+
+namespace Sulakore.Communication
+{
+    public class HostKickedPlayerEventArgs : EventArgs, IHabboEvent
+    {
+        private readonly HMessage _packet;
+
+        public ushort Header { get; private set; }
+
+        private int? _playerId;
+        public int PlayerId
+        {
+            get
+            {
+                return (int)(_playerId != null ?
+                    _playerId :
+                    _playerId = _packet.ReadInt(0));
+            }
+        }
+
+        public HostKickedPlayerEventArgs(HMessage packet)
+        {
+            _packet = packet;
+            Header = _packet.Header;
+        }
+    }
+}
