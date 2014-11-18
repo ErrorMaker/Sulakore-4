6455eb5392ab8c0aaf552aa816c56da871d319ee
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:17:15 -0800

    Renamed HostTradeEventArgs to HostTradePlayerEventArgs.

---
+++ Sulakore/Communication/Host Event Arguments/HostTradePlayerEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sulakore.Protocol;
+
+namespace Sulakore.Communication
+{
+    public class HostTradePlayerEventArgs : EventArgs, IHabboEvent
+    {
+        private readonly HMessage _packet;
+
+        public ushort Header { get; private set; }
+
+        private int? _playerIndex;
+        public int PlayerIndex
+        {
+            get
+            {
+                return (int)(_playerIndex != null ?
+                    _playerIndex :
+                    _playerIndex = _packet.ReadInt(0));
+            }
+        }
+
+        public HostTradePlayerEventArgs(HMessage packet)
+        {
+            _packet = packet;
+            Header = _packet.Header;
+        }
+    }
+}
