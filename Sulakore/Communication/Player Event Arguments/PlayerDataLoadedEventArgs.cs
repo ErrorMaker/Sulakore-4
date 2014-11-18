94cf747d3acec456403aaa6d0079e56596b3aa75
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:11:54 -0800

    Added new event arguments to handle the data of when you are given  information regarding a player in the room, or all of the players in the room.

---
+++ Sulakore/Communication/Player Event Arguments/PlayerDataLoadedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sulakore.Habbo;
+using Sulakore.Protocol;
+using System.Collections.Generic;
+
+namespace Sulakore.Communication
+{
+    public class PlayerDataLoadedEventArgs : EventArgs, IHabboEvent
+    {
+        private readonly HMessage _packet;
+        private readonly IEnumerable<IHPlayerData> _playerData;
+
+        public ushort Header { get; private set; }
+
+        public PlayerDataLoadedEventArgs(HMessage packet)
+        {
+            _packet = packet;
+            Header = _packet.Header;
+
+            _playerData = HPlayerData.Extract(packet);
+        }
+    }
+}
