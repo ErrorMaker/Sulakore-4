5f57275298e0881c6505d59a1453c73812f39982
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:16:28 -0800

    Renamed HostNavigateEventArgs to HostRoomNavigateEventArgs.cs

---
+++ Sulakore/Communication/Host Event Arguments/HostRoomNavigateEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Sulakore.Protocol;
+
+namespace Sulakore.Communication
+{
+    public class HostRoomNavigateEventArgs : EventArgs, IHabboEvent
+    {
+        private readonly HMessage _packet;
+
+        public ushort Header { get; private set; }
+
+        private int? _roomId;
+        public int RoomId
+        {
+            get
+            {
+                return (int)(_roomId != null ?
+                    _roomId :
+                    _roomId = _packet.ReadInt(0));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password != null ?
+                    _password :
+                    _password = _packet.ReadString(4);
+            }
+        }
+
+        public HostRoomNavigateEventArgs(HMessage packet)
+        {
+            _packet = packet;
+            Header = _packet.Header;
+        }
+    }
+}
