1843ede47babab915cafcba27a087f083dd27ab0
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:15:35 -0800

    Renamed HostSignEventArgs to HostRaiseSignEventArgs.

---
+++ Sulakore/Communication/Host Event Arguments/HostRaiseSignEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sulakore.Habbo;
+using Sulakore.Protocol;
+
+namespace Sulakore.Communication
+{
+    public class HostRaiseSignEventArgs : EventArgs, IHabboEvent
+    {
+        private readonly HMessage _packet;
+
+        public ushort Header { get; private set; }
+
+        private HSigns? _sign;
+        public HSigns Sign
+        {
+            get
+            {
+                return (HSigns)(_sign != null ?
+                    _sign :
+                    _sign = (HSigns)_packet.ReadInt(0));
+            }
+        }
+
+        public HostRaiseSignEventArgs(HMessage packet)
+        {
+            _packet = packet;
+            Header = _packet.Header;
+        }
+    }
+}
