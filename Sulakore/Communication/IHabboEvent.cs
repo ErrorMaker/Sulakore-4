594d7afa8d6d881167ac797cd07410727abe908e
ArachisH <TheOldNutMan@gmail.com>
2014-11-17 16:10:38 -0800

    Reduced the amount of properties that the IHabboEvent interface provides.

--- Sulakore/Communication/IHabboEvent.cs
+++ Sulakore/Communication/IHabboEvent.cs
@@ -1,12 +1,7 @@
-﻿using Sulakore.Protocol;
-using System.Collections.Generic;
-
-namespace Sulakore.Communication
+﻿namespace Sulakore.Communication
 {
     public interface IHabboEvent
     {
-        Dictionary<string, object> Data { get; }
-        HMessage Packet { get; }
         ushort Header { get; }
     }
 }
