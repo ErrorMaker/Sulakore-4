aa4487617ac4e301b76bba38d11d0b4e997674e2
ArachisH <TheOldNutMan@gmail.com>
2014-11-30 10:04:08 -0800

    Added new enum object to Sulakore namespace; HSpeeches.

--- Sulakore/Habbo/Enumerators.cs
+++ Sulakore/Habbo/Enumerators.cs
@@ -405,6 +405,25 @@
     }
 
     /// <summary>
+    /// Specifies the different types of speech modes that players can communicate with each other in-game.
+    /// </summary>
+    public enum HSpeeches
+    {
+        /// <summary>
+        /// Represents a speech mode that makes the message publicly visible within a determined range in the room.
+        /// </summary>
+        Say = 0,
+        /// <summary>
+        /// Represents a speech mode that makes the message public to everyone in the room.
+        /// </summary>
+        Shout = 1,
+        /// <summary>
+        /// Represents a speech mode that only makes the message visible to the specified target regardless of the range.
+        /// </summary>
+        Whisper = 2
+    }
+
+    /// <summary>
     /// Specifies the different types of choices given on the page represented by Sulakore.HPages.Profile.
     /// </summary>
     public enum HVisibility
